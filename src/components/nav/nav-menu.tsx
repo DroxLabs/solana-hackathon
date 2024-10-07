d420ed60af57cf05a800b5245818a39822a98bc1
Syed Muqeet Aqib <smuqeetaqib@gmail.com>
2024-10-07 21:11:03 +0500

    add logo

--- src/components/nav/nav-menu.tsx
+++ src/components/nav/nav-menu.tsx
@@ -31,7 +31,7 @@ const menuItems = [
     ],
   },
   {
-    label: "Multisend",
+    label: "Batch Transactions",
     link: "/",
   },
   // {
--- src/components/ui/logo-ui/logo.tsx
+++ src/components/ui/logo-ui/logo.tsx
@@ -1,10 +1,17 @@
+"use client";
+import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
   return (
-    <div className="text-lg lg:text-2xl font-extrabold text-gray-200">
-      Multisender
-    </div>
+    <Link href="/">
+      <div
+        className="text-lg lg:text-2xl font-extrabold text-gray-200"
+        title="Batch transfer transactions up to 22 transfers in one transactions"
+      >
+        Batch Transactions
+      </div>
+    </Link>
   );
 };
 
